043c5f249ef335ae1a0aab380e6b19c98441d870
Aboloooo <abolfazle0915501@gmail.com>
2025-04-22 08:37:01 +0200

    ewrg

--- Database/tables.sql
+++ Database/tables.sql
@@ -2,18 +2,22 @@ CREATE DATABASE IF NOT EXISTS Don_Bosco;
 USE Don_Bosco;
 
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     UserID INT PRIMARY KEY AUTO_INCREMENT,
     Username VARCHAR(255),
     Password VARCHAR(255),
     First_name VARCHAR(255),
     Last_name VARCHAR(255),
     Email VARCHAR(255),
-    Level VARCHAR(255)
+    Level VARCHAR(255),
+    user_must_change_password boolean;
+    /* Zero is considered as false, nonzero values are considered as true.  */
 );
 
-INSERT INTO users (Username, Password, First_name, Last_name, Email, Level)
-VALUES ('admin', 'password', 'abolo', 'ahmadi', 'abolo@gmail.com', 'admin');
+INSERT INTO users (Username, Password, First_name, Last_name, Email, Level, user_must_change_password)
+VALUES
+('admin', 'password', 'abolo', 'ahmadi', 'abolo@gmail.com', 'admin', 0), 
+('abolo123', '123', 'abolo', 'ahmadi', 'abolo@gmail.com', 'user', 1);
 
 CREATE TABLE IF NOT EXISTS reservation (
     ReservationID INT PRIMARY KEY AUTO_INCREMENT,

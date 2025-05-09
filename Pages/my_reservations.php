923a82ee2467d4c11cfb1d66f3006415879743de
abolo <abolfazle0915501@gmail.com>
2025-05-09 11:26:10 +0200

    .kn

--- Pages/my_reservations.php
+++ Pages/my_reservations.php
@@ -16,84 +16,107 @@ include_once("../Library/MyLibrary.php");
   <?= NavBar('my_reservations') ?>
 
   <?php
-  if (isset($_POST['cancel'])) {
-    $cancelDateTime = $_POST['datetime'];
-    // $session username makes an error here
-    $delete = $connection->prepare("DELETE FROM reservation WHERE StartMoment = ? AND Reserved_by_userID = (SELECT UserID FROM users WHERE Username = ?)");
-    $delete->bind_param('ss', $cancelDateTime, $_SESSION['username']);
-    if ($delete->execute()) {
-      echo "<script>alert('Reservation cancelled successfully');</script>";
-    } else {
-      echo "<script>alert('Cancellation failed');</script>";
-    }
-  }
 
   $userInfo = $connection->prepare('SELECT * FROM users WHERE Username = ?');
   $userInfo->bind_param('s', $_SESSION['username']);
+  $AdminMode = false;
 
   if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
   } else {
-    $userInfo->execute();
-    $userInfoResult = $userInfo->get_result();
-    $userRow = $userInfoResult->fetch_assoc();
+    if ($_SESSION['username'] !== "Unknown") {
+      $userInfo->execute();
+      $userInfoResult = $userInfo->get_result();
+      $userRow = $userInfoResult->fetch_assoc();
 
-    if (strtoupper($userRow['Level']) === 'ADMIN') {
-      $displayReservations = $connection->prepare('SELECT * FROM reservation');
-    } else {
-      $displayReservations = $connection->prepare('SELECT * FROM reservation WHERE Reserved_by_userID = ?');
-      $displayReservations->bind_param('i', $userRow['UserID']);
-    }
+      if (strtoupper($userRow['Level']) === 'ADMIN') {
+        $AdminMode = true;
+        $displayReservations = $connection->prepare('SELECT * FROM reservation');
+      } else {
+        $displayReservations = $connection->prepare('SELECT * FROM reservation WHERE Reserved_by_userID = ?');
+        $displayReservations->bind_param('i', $userRow['UserID']);
+      }
 
-    $displayReservations->execute();
-    $reservations = $displayReservations->get_result();
+      $displayReservations->execute();
+      $reservations = $displayReservations->get_result();
 
-    if ($reservations->num_rows > 0) {
+      if ($reservations->num_rows > 0) {
+
+        if (isset($_POST['cancel'])) {
+          $cancelDateTime = $_POST['datetime'];
+          // $session username makes an error here
+          // $delete = $connection->prepare("DELETE FROM reservation WHERE StartMoment = ? AND Reserved_by_userID = (SELECT UserID FROM users WHERE Username = ?)");
+          if ($AdminMode) {
+            $queary = "DELETE FROM reservation WHERE StartMoment = ?";
+            $delete = $connection->prepare($queary);
+            $delete->bind_param('s', $cancelDateTime);
+          } else {
+            $queary = "DELETE FROM reservation WHERE StartMoment = ? AND Reserved_by_userID = (SELECT UserID FROM users WHERE Username = ?)";
+            $delete = $connection->prepare($queary);
+            $delete->bind_param('ss', $cancelDateTime, $_SESSION['username']);
+          }
+          if ($delete->execute()) {
+            echo "<script>alert('Reservation cancelled successfully');</script>";
+            header("Refresh:0");
+          } else {
+            echo "<script>alert('Cancellation failed');</script>";
+          }
+        }
   ?>
 
-      <div class="my_reservation_container">
-        <h1>My Kitchen Reservations</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Date</th>
-              <th>Time Slot</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-            $i = 1;
-            while ($row = $reservations->fetch_assoc()) {
-              $startMoment = $row['StartMoment'];
-              $reservedDate = substr($startMoment, 0, 10);
-              $reservedTime = substr($startMoment, 11);
-              $startTime = new DateTime($reservedTime);
-              $endTime = clone $startTime;
-              $endTime->modify('+3 hours');
-
-              $timeSlot = $startTime->format('H:i') . ' - ' . $endTime->format('H:i');
-            ?>
+        <div class="my_reservation_container">
+          <h1>My Kitchen Reservations</h1>
+          <table>
+            <thead>
               <tr>
-                <td><?= $i++ ?></td>
-                <td><?= $reservedDate ?></td>
-                <td><?= $timeSlot ?></td>
-                <td>
-                  <form method="POST" onsubmit="return confirm('Are you sure you want to cancel this reservation?');">
-                    <input type="hidden" name="datetime" value="<?= $startMoment ?>">
-                    <button type="submit" class="cancel-btn" name="cancel">Cancel</button>
-                  </form>
-                </td>
+
+                <th>#</th>
+                </a>
+                <?= ($AdminMode) ? '<th>Reserved by user_ID</th>' : ''; ?>
+                <th>Date</th>
+                <th>Time Slot</th>
+                <th>Action</th>
               </tr>
-            <?php } ?>
-          </tbody>
-        </table>
-      </div>
-    <?php
+            </thead>
+            <tbody>
+              <?php
+              $i = 1;
+              while ($row = $reservations->fetch_assoc()) {
+                $user_ID = $row['Reserved_by_userID'];
+                $startMoment = $row['StartMoment'];
+                $reservedDate = substr($startMoment, 0, 10);
+                $reservedTime = substr($startMoment, 11);
+                $startTime = new DateTime($reservedTime);
+                $endTime = clone $startTime;
+                $endTime->modify('+3 hours');
+
+                $timeSlot = $startTime->format('H:i') . ' - ' . $endTime->format('H:i');
+              ?>
+                <tr>
+                  <td><?= $i++ ?></td>
+                  <?= ($AdminMode) ? '<td>' . $user_ID . '</td>' : ''; ?>
+                  <td><?= $reservedDate ?></td>
+                  <td><?= $timeSlot ?></td>
+                  <td>
+                    <form method="POST" onsubmit="return confirm('Are you sure you want to cancel this reservation?');">
+                      <input type="hidden" name="datetime" value="<?= $startMoment ?>">
+                      <button type="submit" class="cancel-btn" name="cancel">Cancel</button>
+                    </form>
+                  </td>
+                </tr>
+              <?php } ?>
+            </tbody>
+          </table>
+        </div>
+      <?php
+      } else {
+      ?>
+        <div class="no-reservations">You have no reservations yet.</div>
+      <?php
+      }
     } else {
-    ?>
-      <div class="no-reservations">You have no reservations yet.</div>
+      ?>
+      <div class="no-reservations">Please Login to display the reservations.</div>
   <?php
     }
   }
